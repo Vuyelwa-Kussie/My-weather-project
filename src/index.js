0b5f7811e6a7af28de47616864aebd5f0d8fe06d
Vuyelwa-Kussie <155361771+Vuyelwa-Kussie@users.noreply.github.com>
2024-03-12 22:38:24 +0200

    Imported the weather API and made app display current temperature of city being searched

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
+function refreshWeather(response) {
+  let tempElement = document.querySelector(".temp-value");
+  let temperature = response.data.temperature.current;
+  let city = document.querySelector(".city");
+  city.innerHTML = response.data.city;
+  tempElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apiKey = "30417c04bdtca9bb49d2o565a2bfd928";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function handleSearchForm(event) {
   event.preventDefault();
   let searchInput = document.querySelector(".search-input");
-  let city = document.querySelector(".city");
-  city.innerHTML = searchInput.value;
+
+  searchCity(searchInput.value);
 }
 
-let searchCity = document.querySelector(".search-form");
-searchCity.addEventListener("submit", handleSearchForm);
+let searchCityForm = document.querySelector(".search-form");
+searchCityForm.addEventListener("submit", handleSearchForm);
+
+searchCity("Cape Town");
