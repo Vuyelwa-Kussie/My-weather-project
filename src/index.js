c1ad4b15df62af3a137bc5d23da0705ec4642c13
Vuyelwa-Kussie <155361771+Vuyelwa-Kussie@users.noreply.github.com>
2024-03-13 11:54:58 +0200

    Updates the time, date and icon elements in my JS code and HTML code

--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ function refreshWeather(response) {
   let description = document.querySelector(".conditions");
   let humidity = document.querySelector(".humidity");
   let windSpeed = document.querySelector(".wind-speed");
+  let dateTime = document.querySelector(".date-time");
+  let icon = document.querySelector(".temp-icon");
 
   let date = new Date(response.data.time * 1000);
   let temperature = response.data.temperature.current;
@@ -13,9 +15,48 @@ function refreshWeather(response) {
   description.innerHTML = response.data.condition.description;
   humidity.innerHTML = `${response.data.temperature.humidity}%`;
   windSpeed.innerHTML = `${response.data.wind.speed}km/h`;
+  dateTime.innerHTML = formatDate(date);
+  icon.innerHTML = `<img src="${response.data.condition.icon_url}"/>`;
 }
 
-function formatDate(date) {}
+function formatDate(date) {
+  let currentDate = date.getDate();
+  let hours = date.getHours();
+  let minutes = date.getMinutes();
+
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+  let days = [
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+    "Sunday",
+  ];
+
+  let month = months[date.getMonth()];
+  let day = days[date.getDay()];
+
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${day}, ${month} ${currentDate} <br / > ${hours}:${minutes}`;
+}
 
 function searchCity(city) {
   let apiKey = "30417c04bdtca9bb49d2o565a2bfd928";
